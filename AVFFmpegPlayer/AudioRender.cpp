d2295850293fe7ec23fe53e969fb03e207707f3a
wangyonglin <admin@wangyonglin.com>
2024-05-18 23:06:00 +0800

    测试成功

--- AVFFmpegPlayer/AudioRender.cpp
+++ AVFFmpegPlayer/AudioRender.cpp
@@ -1,244 +1,50 @@
-#include "AudioRender.h"
+#include "AudioOutput.h"
 
-
-AudioRender::AudioRender(QObject *parent)
-    : AudioOutput{parent}
-
+AudioOutput::AudioOutput(QObject *parent)
+    : QObject{parent}
 {
 
 }
 
-AudioRender::~AudioRender()
+AudioOutput::~AudioOutput()
 {
 
 }
 
 
-int  AudioRender::InitSwrResample(int64_t src_ch_layout, int64_t dst_ch_layout,
-                                 int src_rate, int dst_rate,
-                                 enum AVSampleFormat src_sample_fmt, enum AVSampleFormat dst_sample_fmt,
-                                 int src_nb_samples)
+
+void AudioOutput::InitFormat(int dst_nb_samples, int rate, int sample_size, int nch)
 {
 
-    src_sample_fmt_ = src_sample_fmt;
-    dst_sample_fmt_ = dst_sample_fmt;
+    QAudioFormat format;
+    format.setSampleRate(rate); // 采样率
+    format.setChannelCount(nch);   // 声道数
+    format.setSampleSize(sample_size);    // 采样大小
+    format.setCodec("audio/pcm"); // 音频编码格式
+    format.setByteOrder(QAudioFormat::LittleEndian); // 字节顺序
+    format.setSampleType(QAudioFormat::SignedInt);  // 采样类型
 
-    int ret;
-    /* create resampler context */
-    swr_ctx = swr_alloc();
-    if (!swr_ctx) {
-        qDebug()  << "Could not allocate resampler context";
-        ret = AVERROR(ENOMEM);
-        return ret;
-    }
+    int len = dst_nb_samples * format.channelCount() * format.sampleSize()/8;
+    Q_UNUSED(len);
+    // 创建 QAudioOutput 对象
+    audioOutput = new QAudioOutput(format);
+    //audioOutput->setBufferSize(len * 100);
+    audioOutput->setVolume(1.0); // 设置音量（0.0 - 1.0）
 
-    /* set options */
-    av_opt_set_int(swr_ctx, "in_channel_layout", src_ch_layout, 0);
-    av_opt_set_int(swr_ctx, "in_sample_rate", src_rate, 0);
-    av_opt_set_sample_fmt(swr_ctx, "in_sample_fmt", src_sample_fmt, 0);
+    // 打开音频输出
+    outputDevice = audioOutput->start();
+}
 
-    av_opt_set_int(swr_ctx, "out_channel_layout", dst_ch_layout, 0);
-    av_opt_set_int(swr_ctx, "out_sample_rate", dst_rate, 0);
-    av_opt_set_sample_fmt(swr_ctx, "out_sample_fmt", dst_sample_fmt, 0);
+void AudioOutput::WriteOutput(const char *data, qint64 len)
+{
+    outputDevice->write(data, len);
 
-    /* initialize the resampling context */
-    if ((ret = swr_init(swr_ctx)) < 0) {
-        qDebug()  << "Failed to initialize the resampling context";
-        return -1;
-    }
+}
 
-    //配置输入的参数
-    /*
-    * src_nb_samples: 描述一整的采样个数 比如这里就是 1024
-    * src_linesize: 描述一行采样字节长度
-    *   当是planr 结构 LLLLLRRRRRR 的时候 比如 一帧1024个采样，32位表示。那就是 1024*4 = 4096
-    *   当是非palner 结构的时候 LRLRLR 比如一帧1024采样 32位表示 双通道   1024*4*2 = 8196 要乘以通道
-    * src_nb_channels : 可以根据布局获得音频的通道
-    * ret 返回输入数据的长度 比如这里 1024 * 4 * 2 = 8196 (32bit，双声道，1024个采样)
-    */
-    src_nb_channels = av_get_channel_layout_nb_channels(src_ch_layout);
-
-    ret = av_samples_alloc_array_and_samples(&src_data_, &src_linesize, src_nb_channels,
-                                             src_nb_samples, src_sample_fmt, 0);
-    if (ret < 0) {
-        qDebug()  << "Could not allocate source samples\n";
-        return -1;
-    }
-    src_nb_samples_ = src_nb_samples;
-
-    //配置输出的参数
-    int max_dst_nb_samples = dst_nb_samples_ =
-        av_rescale_rnd(src_nb_samples, dst_rate, src_rate, AV_ROUND_UP);
-
-    dst_nb_channels = av_get_channel_layout_nb_channels(dst_ch_layout);
-
-    ret = av_samples_alloc_array_and_samples(&dst_data_, &dst_linesize, dst_nb_channels,
-                                             dst_nb_samples_, dst_sample_fmt, 0);
-    if (ret < 0) {
-        qDebug()  << "Could not allocate destination samples";
-        return -1;
-    }
-
-    int data_size = av_get_bytes_per_sample(dst_sample_fmt_);
-    InitFormat(dst_nb_samples_, dst_rate, data_size*8, dst_nb_channels);
+void AudioOutput::FreeFormat()
+{
+     audioOutput->stop();
 }
 
 
-int  AudioRender::InitSwrResample(AVCodecContext * dec_ctx,
-                                 int64_t dst_ch_layout,
-                                 int dst_rate,
-                                 enum AVSampleFormat dst_sample_fmt)
-{
-
-    int64_t src_ch_layout=dec_ctx->channel_layout;
-    int  src_rate=dec_ctx->sample_rate;
-    enum AVSampleFormat src_sample_fmt=dec_ctx->sample_fmt;
-    int src_nb_samples=dec_ctx->frame_size;
-    src_sample_fmt_ = dec_ctx->sample_fmt;
-    dst_sample_fmt_ = dst_sample_fmt;
-
-    int ret;
-    /* create resampler context */
-    swr_ctx = swr_alloc();
-    if (!swr_ctx) {
-        qDebug()  << "Could not allocate resampler context";
-        ret = AVERROR(ENOMEM);
-        return ret;
-    }
-
-    /* set options */
-    av_opt_set_int(swr_ctx, "in_channel_layout", src_ch_layout, 0);
-    av_opt_set_int(swr_ctx, "in_sample_rate", src_rate, 0);
-    av_opt_set_sample_fmt(swr_ctx, "in_sample_fmt", dec_ctx->sample_fmt, 0);
-
-    av_opt_set_int(swr_ctx, "out_channel_layout", dst_ch_layout, 0);
-    av_opt_set_int(swr_ctx, "out_sample_rate", dst_rate, 0);
-    av_opt_set_sample_fmt(swr_ctx, "out_sample_fmt", dst_sample_fmt, 0);
-
-    /* initialize the resampling context */
-    if ((ret = swr_init(swr_ctx)) < 0) {
-        qDebug()  << "Failed to initialize the resampling context";
-        return -1;
-    }
-
-    //配置输入的参数
-    /*
-    * src_nb_samples: 描述一整的采样个数 比如这里就是 1024
-    * src_linesize: 描述一行采样字节长度
-    *   当是planr 结构 LLLLLRRRRRR 的时候 比如 一帧1024个采样，32位表示。那就是 1024*4 = 4096
-    *   当是非palner 结构的时候 LRLRLR 比如一帧1024采样 32位表示 双通道   1024*4*2 = 8196 要乘以通道
-    * src_nb_channels : 可以根据布局获得音频的通道
-    * ret 返回输入数据的长度 比如这里 1024 * 4 * 2 = 8196 (32bit，双声道，1024个采样)
-    */
-    src_nb_channels = av_get_channel_layout_nb_channels(src_ch_layout);
-
-    ret = av_samples_alloc_array_and_samples(&src_data_, &src_linesize, src_nb_channels,
-                                             src_nb_samples, src_sample_fmt, 0);
-    if (ret < 0) {
-        qDebug()  << "Could not allocate source samples\n";
-        return -1;
-    }
-    src_nb_samples_ = src_nb_samples;
-
-    //配置输出的参数
-    int max_dst_nb_samples = dst_nb_samples_ =
-        av_rescale_rnd(src_nb_samples, dst_rate, src_rate, AV_ROUND_UP);
-
-    dst_nb_channels = av_get_channel_layout_nb_channels(dst_ch_layout);
-
-    ret = av_samples_alloc_array_and_samples(&dst_data_, &dst_linesize, dst_nb_channels,
-                                             dst_nb_samples_, dst_sample_fmt, 0);
-    if (ret < 0) {
-        qDebug()  << "Could not allocate destination samples";
-        return -1;
-    }
-
-    int data_size = av_get_bytes_per_sample(dst_sample_fmt_);
-    InitFormat(dst_nb_samples_, dst_rate, data_size*8, dst_nb_channels);
-}
-
-int AudioRender::WriteInput(AVFrame* frame)
-{
-    int planar = av_sample_fmt_is_planar(src_sample_fmt_);
-    int data_size = av_get_bytes_per_sample(src_sample_fmt_);
-    if (planar)
-    {
-
-        //src是planar类型的话，src_data里面数据是LLLLLLLRRRRR 结构，src_data_[0] 指向全部的L，src_data_[1] 指向全部R
-        // src_data_ 里面其实一个长 uint8_t *buf，src_data_[0] 指向L开始的位置，src_data_[1]指向R的位置
-        // linesize 是 b_samples * sample_size 就是比如 48000*4
-        for (int ch = 0; ch < src_nb_channels; ch++) {
-            memcpy(src_data_[ch], frame->data[ch], data_size * frame->nb_samples);
-        }
-    }
-    else
-    {
-        //src是非planar类型的话，src_data里面数据是LRLRLRLR 结构，src_data_[0] 指向全部数据 没有src_data[1]
-        // linesize 是nb_samples * sample_size * nb_channels 比如 48000*4*2
-        for (int i = 0; i < frame->nb_samples; i++){
-            for (int ch = 0; ch < src_nb_channels; ch++)
-            {
-                memcpy(src_data_[0], frame->data[ch]+data_size*i, data_size);
-            }
-        }
-    }
-    return 0;
-}
-
-
-int AudioRender::SwrConvert()
-{
-    int ret = swr_convert(swr_ctx, dst_data_, dst_nb_samples_, (const uint8_t**)src_data_, src_nb_samples_);
-    if (ret < 0) {
-        fprintf(stderr, "Error while converting\n");
-        exit(1);
-    }
-
-    int  dst_bufsize = av_samples_get_buffer_size(&dst_linesize, dst_nb_channels,
-                                                 ret, dst_sample_fmt_, 1);
-
-    int planar = av_sample_fmt_is_planar(dst_sample_fmt_);
-    if (planar)
-    {
-
-        int data_size = av_get_bytes_per_sample(dst_sample_fmt_);
-
-
-
-    }
-    else {
-        //非planr结构，dst_data_[0] 里面存在着全部数据
-
-        WriteOutput((const char*)(dst_data_[0]), dst_bufsize);
-    }
-
-    return dst_bufsize;
-}
-
-void AudioRender::FreeSwrResample()
-{
-
-    if (src_data_)
-        av_freep(&src_data_[0]);
-
-    av_freep(&src_data_);
-
-    if (dst_data_)
-        av_freep(&dst_data_[0]);
-
-    av_freep(&dst_data_);
-
-    swr_free(&swr_ctx);
-
-    FreeFormat();
-}
-
-bool AudioRender::initSuccessful()
-{
-    if(swr_ctx){
-        return true;
-    }
-    return false;
-
-}
 
--- AVFFmpegPlayer/AudioRender.h
+++ AVFFmpegPlayer/AudioRender.h
@@ -1,61 +1,34 @@
-#ifndef AUDIORENDER_H
+#ifndef AUDIOOUTPUT_H
+#define AUDIOOUTPUT_H
 
-#define AUDIORENDER_H
 
-extern "C" {
-#include <libavutil/pixfmt.h>
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libavutil/avutil.h"
-#include "libswscale/swscale.h"
-#include "libavutil/imgutils.h"
-#include <libavutil/opt.h>
-#include <libavutil/channel_layout.h>
-#include <libavutil/samplefmt.h>
-#include <libavutil/time.h>
-#include <libswresample/swresample.h>
-}
-
-#include "AudioOutput.h"
 #include <QApplication>
 #include <QAudioOutput>
 #include <QIODevice>
 #include <QDebug>
 #include <QObject>
-#include <QByteArray>
+#include <QWidget>
+#include <chrono>
+#include <QPaintEvent>
+#include <QPainter>
 
-class AudioRender : public AudioOutput
+class AudioOutput: public QObject
 {
     Q_OBJECT
 public:
-    explicit AudioRender(QObject *parent = nullptr);
-    ~AudioRender();
-    int InitSwrResample(int64_t src_ch_layout,
-                        int64_t dst_ch_layout,
-                        int src_rate,
-                        int dst_rate,
-                        AVSampleFormat src_sample_fmt,
-                        AVSampleFormat dst_sample_fmt,
-                        int src_nb_samples);
+    explicit AudioOutput(QObject *parent = nullptr);
+    ~AudioOutput();
 
-    int WriteInput(AVFrame *frame);
-    int SwrConvert();
-    void FreeSwrResample();
-    bool initSuccessful();
-
-    int InitSwrResample(AVCodecContext *dec_ctx,
-                        int64_t dst_ch_layout,
-                        int dst_rate,
-                        AVSampleFormat dst_sample_fmt);
+    void InitFormat(int dst_nb_samples, int rate, int sample_size, int nch);
+    void WriteOutput(const char* data, qint64 len);
+    void FreeFormat();
+protected:
 private:
-    struct SwrContext* swr_ctx;
-    uint8_t** src_data_;
-    uint8_t** dst_data_;
-    int src_nb_channels, dst_nb_channels;
-    int src_linesize, dst_linesize;
-    int src_nb_samples_, dst_nb_samples_;
-    enum AVSampleFormat dst_sample_fmt_;
-    enum AVSampleFormat src_sample_fmt_;
+    QIODevice* outputDevice;
+    QAudioOutput* audioOutput;
+    int64_t player_start_time_ms = 0;
+
+signals:
 };
 
-#endif // AUDIORENDER_H
+#endif // AUDIOOUTPUT_H
